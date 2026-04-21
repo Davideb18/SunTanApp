78db313023d85181dc1d377754057aab739170a5
Davide <davide@suntanapp.dev>
2026-04-21 14:37:27 +0200

    feat: integrate NativeWind v4 with Tailwind CSS and configure project dependencies

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+          reanimated: false, // Disabilita il plugin interno di Expo per evitare duplicati con NativeWind v4
+        },
+      ],
+      "nativewind/babel",
     ],
-    plugins: ["nativewind/babel"],
   };
 };
